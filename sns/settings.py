4ed1160a45acf8788bfca7476b79327303d87e9f
k-kw <kouki1212@chiba-u.jp>
2022-10-02 20:51:29 +0900

    allowed host

--- sns/settings.py
+++ sns/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['Django_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ['Django_DEBUG']))
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
